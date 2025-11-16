f14e87e5495fbdab1e16a56687701a518621e6b3
anujkumarv965-dotcom <anujkumarv965@gmail.com>
2025-11-16 19:57:48 +0530

    chore(android): add MainActivity

---
+++ app/src/main/java/com/sevasathi/app/MainActivity.java
@@ -0,0 +1,35 @@
+package com.sevasathi.app;
+
+import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+
+        WebSettings ws = webView.getSettings();
+        ws.setJavaScriptEnabled(true);
+        ws.setDomStorageEnabled(true);
+        ws.setGeolocationEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
+
+        webView.loadUrl("https://sevasathi.onrender.com");
+    }
+}￼Enter
